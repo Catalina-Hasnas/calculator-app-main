d32c95c1045010161bfb5e52fdeb5109f1e37420
Catalina <catalinahasnas1996@gmail.com>
2023-10-10 20:41:44 +0300

    add calculator styling and theme logic

--- javascript/theme.js
+++ javascript/theme.js
@@ -1,8 +1,48 @@
-const switcher = document.querySelector("#theme-switcher");
+const togglerButton = document.querySelector("#toggler-button");
 
 const doc = document.firstElementChild;
 
 const setTheme = (theme) => doc.setAttribute("color-scheme", theme);
 
-if (switcher)
-  switcher.addEventListener("input", (e) => setTheme(e.target.value));
+let circlePosition = undefined;
+
+const circlePositionToThemeValueMapper = {
+  1: { theme: "default", leftPosition: "20%" },
+  2: { theme: "light", leftPosition: "50%" },
+  3: { theme: "dark", leftPosition: "80%" },
+};
+
+const mapCirclePositionToThemeValue = () => {
+  circlePosition = !circlePosition ? 1 : circlePosition;
+
+  const currentValue = circlePositionToThemeValueMapper[circlePosition].theme;
+
+  const selectedRadio = document.querySelector(
+    `input[type="radio"][value="${currentValue}"]`
+  );
+  const previouslySelectedRadio = document.querySelector(
+    `input[type="radio"][checked=""]`
+  );
+  selectedRadio.setAttribute("checked", "");
+  if (previouslySelectedRadio) {
+    previouslySelectedRadio.removeAttribute("checked");
+  }
+  const circle = document.querySelector("#circle");
+  circle.style.left =
+    circlePositionToThemeValueMapper[circlePosition].leftPosition;
+  setTheme(currentValue);
+};
+
+const onTogglerButtonClick = (event) => {
+  event.preventDefault();
+  if (circlePosition === 3) {
+    circlePosition = 1;
+  } else {
+    circlePosition++;
+  }
+  mapCirclePositionToThemeValue();
+};
+
+mapCirclePositionToThemeValue();
+
+togglerButton.addEventListener("click", onTogglerButtonClick);
